7082d1011bae3970d60728904674c81cae925622
Ryo Nakamura <r7kamura@gmail.com>
2021-11-17 22:01:48 +0900

    Add Error variants for client

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,21 @@
 #[derive(Debug)]
 pub enum Error {
+    ActixPayload(actix_web::error::PayloadError),
+    ActixRequest(actix_web::client::SendRequestError),
     Io(std::io::Error),
-    Request(actix_web::client::SendRequestError),
+    String(std::string::FromUtf8Error),
+}
+
+impl From<actix_web::error::PayloadError> for Error {
+    fn from(error: actix_web::error::PayloadError) -> Self {
+        Error::ActixPayload(error)
+    }
+}
+
+impl From<actix_web::client::SendRequestError> for Error {
+    fn from(error: actix_web::client::SendRequestError) -> Self {
+        Error::ActixRequest(error)
+    }
 }
 
 impl From<std::io::Error> for Error {
@@ -10,8 +24,8 @@ impl From<std::io::Error> for Error {
     }
 }
 
-impl From<actix_web::client::SendRequestError> for Error {
-    fn from(error: actix_web::client::SendRequestError) -> Self {
-        Error::Request(error)
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(error: std::string::FromUtf8Error) -> Self {
+        Error::String(error)
     }
 }
